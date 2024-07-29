903830f02e5ea46771da2b9d9a95b68c1fe38632
Eduardo San Martin Morote <posva13@gmail.com>
2024-07-29 14:27:43 +0200

    add pinia colada

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 import { DataLoaderPlugin } from 'unplugin-vue-router/data-loaders'
+import { PiniaColada } from '@pinia/colada'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PiniaColada, {})
 app.use(DataLoaderPlugin, { router })
 app.use(router)
 
