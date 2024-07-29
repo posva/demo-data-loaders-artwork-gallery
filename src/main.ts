428b41e5f25c67626ef756be654b36883d3256a7
Eduardo San Martin Morote <posva13@gmail.com>
2024-07-29 14:23:32 +0200

    add tailwaind, uvr

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import './assets/main.css'
+import './style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import { router } from './router'
+import { DataLoaderPlugin } from 'unplugin-vue-router/data-loaders'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(DataLoaderPlugin, { router })
 app.use(router)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     //
+    VueRouter(),
     Vue(),
     VueDevTools(),
   ],
